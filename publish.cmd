ed8234c0f684a10fd48b670122caf9791efd80ab
Antho Pages <anthony.pages@outlook.com>
2018-02-14 12:22:14 +0100

    start

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=deezer-win-Web-Deploy.pubxml -p:Password=r5cMrS5eckfi82KgAWw6dWq2auu8212d3whJhlJ7FdhFGhbrnj41xkWeyuSv
+
